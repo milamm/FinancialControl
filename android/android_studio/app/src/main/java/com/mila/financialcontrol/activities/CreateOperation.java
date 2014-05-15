0e314378441faa1b83aef0bdbbd6b23c144bba20
Mila Maracaba <mila.maracabamoreira@gmail.com>
2014-05-15 20:43:31 -0300

    Add Android Studio Env

---
+++ android/android_studio/app/src/main/java/com/mila/financialcontrol/activities/CreateOperation.java
@@ -0,0 +1,17 @@
+package com.mila.financialcontrol.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mila.financialcontrol.R;
+
+public class CreateOperation extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_operation);
+	}
+
+}
